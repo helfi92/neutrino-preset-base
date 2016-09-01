30714c3010185bd7e3393fac752448218a8f75db
Eli Perelman <eli@eliperelman.com>
2016-09-01 11:34:12 -0500

    Moving karma to web preset

--- config/base.js
+++ config/base.js
@@ -50,55 +50,19 @@ const config = {
         test: /\.js$/,
         include: [SRC],
         loader: 'eslint'
-      }
+      },
+      JS_LOADER
     ],
     loaders: [
       {
         test: /\.json$/,
         include: [SRC, TEST],
         loader: 'json'
-      }
+      },
+      JS_LOADER
     ]
-  },
-  karma: {
-    plugins: [
-      require.resolve('karma-webpack'),
-      require.resolve('karma-chrome-launcher'),
-      require.resolve('karma-coverage'),
-      require.resolve('karma-mocha'),
-      require.resolve('karma-mocha-reporter')
-    ],
-    basePath: CWD,
-    browsers: [process.env.CI ? 'ChromeCI' : 'Chrome'],
-    customLaunchers: {
-      ChromeCI: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
-    frameworks: ['mocha'],
-    files: ['test/**/*_test.js'],
-    preprocessors: {
-      ['test/**/*_test.js']: ['webpack'],
-      ['src/**/*.js']: ['webpack']
-    },
-    webpackMiddleware: { noInfo: true },
-    reporters: ['mocha', 'coverage'],
-    coverageReporter: {
-      dir: '.coverage',
-      reporters: [
-        { type: 'html', subdir: 'report-html' },
-        { type: 'lcov', subdir: 'report-lcov' }
-      ]
-    }
   }
 };
-
-if (process.env.NODE_ENV === 'test') {
-  config.module.preLoaders.push(JS_LOADER);
-} else {
-  config.module.loaders.push(JS_LOADER);
-}
 
 module.exports = config;
 module.exports.CWD = CWD;
